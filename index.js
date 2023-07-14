fc95a23a8b27baeca18d16df93848e185ac34b14
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-14 22:14:55 +0700

    use nodemon module package to auto refresh server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 http.createServer((req,res)=>{
     const data =`
     <h1>Hello perng </h1>
-    <p>congraturation perng </p>
+    <p style="color:green">congraturation perng | 2023</p>
     `
     res.write(data);
     res.end()
