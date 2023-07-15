708ec3d86cf3c5c0b10e4d9a89df1f1cba714285
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-15 22:17:51 +0700

    learn routing and Http status code

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const http = require('http')
+const http = require("http");
 
-http.createServer((req,res)=>{
-    const data =`
-    <h1>Hello perng </h1>
-    <p style="color:green">congraturation perng | 2023</p>
-    `
-    res.write(data);
-    res.end()
-}).listen(8080,'localhost',()=>{
-    console.log("start localhost:8080...!")
-})
+http
+  .createServer((req, res) => {
+    const pathName = req.url;
+    console.log("Url = ", pathName);
+
+    if (pathName === "/" || pathName === "/home") {
+      res.end("<h1>Wellcome Back</h1>");
+    } else if (pathName === "/product") {
+      res.end("<h1>Wellcome to Product</h1>");
+    } else {
+        res.writeHead(404)
+        res.end('<h1>Not Found </h1>')
+    }
+  })
+  .listen(8080, "localhost", () => {
+    console.log("start localhost:8080...!");
+  });
