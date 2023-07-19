8ef2e48c2272c18f4a4d9c9489dddae7a1f88a0e
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-19 21:40:34 +0700

    Html template use module fs to readfile form template to show in server

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
-http
-  .createServer((req, res) => {
+const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`,'utf-8');
+const productPage = fs.readFileSync(`${__dirname}/templates/product1.html`,'utf-8')
+
+http.createServer((req, res) => {
     const pathName = req.url;
     console.log("Url = ", pathName);
 
     if (pathName === "/" || pathName === "/home") {
-      res.end("<h1>Wellcome Back</h1>");
+      res.end(indexPage);
     } else if (pathName === "/product") {
-      res.end("<h1>Wellcome to Product</h1>");
+      res.end(productPage);
     } else {
-        res.writeHead(404)
-        res.end('<h1>Not Found </h1>')
+      res.writeHead(404);
+      res.end("<h1>Not Found </h1>");
     }
   })
   .listen(8080, "localhost", () => {
