6c67ca4e0f38e8d9b515591a360389c85d521e9b
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-26 21:58:24 +0700

    use start Express.js

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express')
+const app = express()
+app.use((res,req)=>{
+  res.send('<h1>Hello Express.js</h1>')
+}).listen(55,()=>{
+  console.log('run server port:55')
+})
 
-const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`,'utf-8');
-const productPage = fs.readFileSync(`${__dirname}/templates/product1.html`,'utf-8')
-
-http.createServer((req, res) => {
-    const pathName = req.url;
-    console.log("Url = ", pathName);
-
-    if (pathName === "/" || pathName === "/home") {
-      res.end(indexPage);
-    } else if (pathName === "/product") {
-      res.end(productPage);
-    } else {
-      res.writeHead(404);
-      res.end("<h1>Not Found </h1>");
-    }
-  })
-  .listen(8080, "localhost", () => {
-    console.log("start localhost:8080...!");
-  });
