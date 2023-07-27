a3606111064842b1b9cd90e87566a7cc63e25889
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-27 21:21:49 +0700

    Express & Routing have 2 : use method and http method(get,post) and i change use to http get method

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-app.use((res,req)=>{
-  res.send('<h1>Hello Express.js</h1>')
-}).listen(55,()=>{
-  console.log('run server port:55')
-})
 
+app.get('/',(req,res)=>{
+  res.send('<h1>Hello Express.js</h1>')
+})
+app.get('/product',(req,res)=>{
+  res.send('<h1>Hello Product</h1>')
+})
+app.listen(55,()=>{
+    console.log('run server port:55')})
