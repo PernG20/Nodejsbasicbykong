de23febaa8b5ecfb517a83a19f5134ef52f94bfa
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-28 22:01:49 +0700

    use module path to read File html change module fs

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+// ກຳນົດ path ຜ່ານຕົວເເປ
+const indexPage = path.join(__dirname,'templates/index.html')
 
 app.get('/',(req,res)=>{
-  res.send('<h1>Hello Express.js</h1>')
+  res.status(200)
+  res.type('text/html')
+  res.sendFile(indexPage)
 })
 app.get('/product',(req,res)=>{
-  res.send('<h1>Hello Product</h1>')
+  res.status(200)
+  res.type('text/html')
+  // ກຳນົດ path ໂດຍຕົງ
+  res.sendFile(path.join(__dirname,'templates/product1.html'))
 })
 app.listen(55,()=>{
     console.log('run server port:55')})
