9bd048b8d4ee6e852f27ace89ebd9aad564ea8cc
PeRnG20 <aiy.jumperest@gmail.com>
2023-07-29 22:15:10 +0700

    module Router use Router() in express

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const express = require('express')
+const router = require('./routes/myroutes')
 const app = express()
-const path = require('path')
-// ກຳນົດ path ຜ່ານຕົວເເປ
-const indexPage = path.join(__dirname,'templates/index.html')
+app.use(router)
 
-app.get('/',(req,res)=>{
-  res.status(200)
-  res.type('text/html')
-  res.sendFile(indexPage)
-})
-app.get('/product',(req,res)=>{
-  res.status(200)
-  res.type('text/html')
-  // ກຳນົດ path ໂດຍຕົງ
-  res.sendFile(path.join(__dirname,'templates/product1.html'))
-})
 app.listen(55,()=>{
     console.log('run server port:55')})
---
+++ routes/myroutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+// const express = require('express')
+router.get('/',(req,res)=>{
+    res.status(200)
+    res.type('text/html')
+    res.sendFile(path.join(__dirname,'../templates/index.html'))
+})
+
+router.get('/product',(req,res)=>{
+    res.status(200)
+    res.type('text/html')
+    res.sendFile(path.join(__dirname,'../templates/product1.html'))
+})
+module.exports = router
